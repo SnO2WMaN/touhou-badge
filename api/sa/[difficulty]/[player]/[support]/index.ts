49e2b6182e3be3daf5f2c030c67e7e07908178d4
SnO₂WMaN <sno2wman.official@gmail.com>
2021-06-15 23:36:27 +0900

    refactor: 地霊殿のバッジ生成のリファクタリング (#5)

--- api/sa/[difficulty]/[player]/[support]/index.ts
+++ api/sa/[difficulty]/[player]/[support]/index.ts
@@ -1,14 +1,19 @@
 import {VercelApiHandler} from '@vercel/node';
 import {makeBadge} from 'badge-maker';
-import {generator, parser} from '../lib/sa';
+import {
+  extractCommonProps,
+  extractOptions,
+  getBadgeProps,
+} from '../../../../../lib/sa';
 
 const handler: VercelApiHandler = (req, res) => {
   try {
-    const option = parser(req.query);
+    const common = extractCommonProps(req.query);
+    const options = extractOptions(req.query);
 
     res.setHeader('Content-Type', 'image/svg+xml');
     res.setHeader('Cache-Control', `public, max-age=${60 * 60 * 12}`);
-    res.send(makeBadge(generator(option)));
+    res.send(makeBadge(getBadgeProps(options, common)));
   } catch {
     res.status(400);
     res.end();
--- lib/sa/index.ts
+++ lib/sa/index.ts
@@ -1,2 +1,131 @@
-export {generator} from './generator';
-export {parser} from './parser';
+import {VercelRequestQuery} from '@vercel/node';
+import {
+  colors,
+  CommonProps,
+  Difficulty,
+  factoryGetBadgeProps,
+  isDifficulty,
+  LabelGenerator,
+  MessageGenerator,
+  selectColor,
+} from '../common';
+
+export {extractCommonProps} from '../common';
+
+export type CharacterReimu = {
+  player: 'reimu';
+  support: 'yukari' | 'suika' | 'aya';
+};
+export type CharacterMarisa = {
+  player: 'marisa';
+  support: 'alice' | 'patchouli' | 'nitori';
+};
+
+export type Options = {
+  difficulty: Difficulty;
+} & (CharacterReimu | CharacterMarisa);
+
+export const isValidCharacter = (value: {
+  player: VercelRequestQuery[string];
+  support: VercelRequestQuery[string];
+}): value is CharacterReimu | CharacterMarisa =>
+  (typeof value.player === 'string' &&
+    typeof value.support === 'string' &&
+    value.player === 'reimu' &&
+    (value.support === 'yukari' ||
+      value.support === 'suika' ||
+      value.support === 'aya')) ||
+  (value.player === 'marisa' &&
+    (value.support === 'alice' ||
+      value.support === 'patchouli' ||
+      value.support === 'nitori'));
+
+export const extractOptions = (query: VercelRequestQuery): Options => {
+  const {difficulty, player, support} = query;
+  const character = {player, support};
+  if (!isDifficulty(difficulty) || !isValidCharacter(character))
+    throw new Error('Invalid query');
+  return {difficulty, ...character};
+};
+
+const generateLabel: LabelGenerator<
+  Options,
+  {
+    game: Record<CommonProps['label'], string>;
+    character: Record<
+      CommonProps['label'],
+      {
+        player: Record<Options['player'], string>;
+        support: Record<Options['support'], string>;
+      }
+    >;
+  }
+> = ({player, support}, {label}, i18n) => {
+  switch (label) {
+    case 'ja-abbr':
+      return `${i18n.game[label]} ${i18n.character[label].player[player]}+${i18n.character[label].support[support]}`;
+    default:
+      return `${i18n.game[label]} ${i18n.character[label].player[player]} & ${i18n.character[label].support[support]}`;
+  }
+};
+
+const generateMessage: MessageGenerator<
+  Options,
+  {
+    difficulty: Record<
+      CommonProps['message'],
+      Record<Options['difficulty'], string>
+    >;
+  }
+> = ({difficulty}, {message}, i18n): string =>
+  i18n.difficulty[message][difficulty];
+
+export const getBadgeProps = factoryGetBadgeProps(
+  generateLabel,
+  generateMessage,
+  selectColor,
+  {
+    game: {'ja-abbr': '地霊殿', 'en-abbr': 'SA'},
+    character: {
+      'ja-abbr': {
+        player: {reimu: '霊夢', marisa: '魔理沙'},
+        support: {
+          yukari: '紫',
+          suika: '萃香',
+          aya: '文',
+          alice: 'アリス',
+          patchouli: 'パチュリー',
+          nitori: 'にとり',
+        },
+      },
+      'en-abbr': {
+        player: {reimu: 'Reimu', marisa: 'Marisa'},
+        support: {
+          yukari: 'Yukari',
+          suika: 'Suika',
+          aya: 'Aya',
+          alice: 'Arice',
+          patchouli: 'Patchouli',
+          nitori: 'Nitori',
+        },
+      },
+    },
+    difficulty: {
+      ja: {
+        easy: 'イージー',
+        normal: 'ノーマル',
+        hard: 'ハード',
+        lunatic: 'ルナティック',
+        extra: 'エクストラ',
+      },
+      en: {
+        easy: 'EASY',
+        normal: 'NORMAL',
+        hard: 'HARD',
+        lunatic: 'LUNATIC',
+        extra: 'EXTRA',
+      },
+    },
+  },
+  colors,
+);
