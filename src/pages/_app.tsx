4f1da19de940db5b3f61a5c16ec66260e3cc8412
SnO2WMaN <me@sno2wman.net>
2021-06-16 17:22:16 +0900

    chore: i18n関連

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
+import {appWithTranslation} from 'next-i18next';
 import {AppProps} from 'next/app';
 import React from 'react';
 import 'tailwindcss/tailwind.css';
+import {NextI18nextConfig} from '~/i18n';
 
 export const App: React.FC<AppProps> = ({Component, pageProps}) => {
   return <Component {...pageProps} />;
 };
 
-export default App;
+export default appWithTranslation(App, NextI18nextConfig);
