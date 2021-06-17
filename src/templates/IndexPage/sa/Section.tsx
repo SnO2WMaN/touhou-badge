1b61b75cfbb38a74a53e6c4631f300e2edc706ea
SnO2WMaN <me@sno2wman.net>
2021-06-17 23:28:14 +0900

    refactor: Context削除

--- src/templates/IndexPage/sa/Section.tsx
+++ src/templates/IndexPage/sa/Section.tsx
@@ -1,18 +1,47 @@
 import clsx from 'clsx';
 import {useTranslation} from 'next-i18next';
 import React, {useMemo, useState} from 'react';
+import {CommonProps} from '~/badges/common';
+import {CharacterMarisa, CharacterReimu, Options} from '~/badges/sa';
 import {badgesBaseUrl} from '~/lib/env';
 import {BadgeBlock} from './Badge';
-import {
-  difficulties,
-  labels,
-  messages,
-  players,
-  styles,
-  supportsMarisa,
-  supportsReimu,
-} from './context';
 import {Selection} from './Selection';
+
+export const difficulties: Record<Options['difficulty'], string> = {
+  easy: 'mof:difficulty.easy',
+  normal: 'mof:difficulty.normal',
+  hard: 'mof:difficulty.hard',
+  lunatic: 'mof:difficulty.lunatic',
+  extra: 'mof:difficulty.extra',
+};
+export const players: Record<Options['player'], string> = {
+  reimu: 'mof:player.reimu',
+  marisa: 'mof:player.marisa',
+};
+export const supportsReimu: Record<CharacterReimu['support'], string> = {
+  yukari: 'sa:support.yukari',
+  suika: 'sa:support.suika',
+  aya: 'sa:support.aya',
+};
+export const supportsMarisa: Record<CharacterMarisa['support'], string> = {
+  alice: 'sa:support.alice',
+  patchouli: 'sa:support.patchouli',
+  nitori: 'sa:support.nitori',
+};
+export const styles: Record<CommonProps['style'], string> = {
+  plastic: 'common:badge.style.plastic',
+  flat: 'common:badge.style.flat',
+  'flat-square': 'common:badge.style.flat-square',
+  'for-the-badge': 'common:badge.style.for-the-badge',
+};
+export const labels: Record<CommonProps['label'], string> = {
+  'ja-abbr': 'common:badge.label.ja',
+  'en-abbr': 'common:badge.label.en',
+};
+export const messages: Record<CommonProps['message'], string> = {
+  ja: 'common:badge.message.ja',
+  en: 'common:badge.message.en',
+};
 
 export type LookProps = {
   className?: string;
